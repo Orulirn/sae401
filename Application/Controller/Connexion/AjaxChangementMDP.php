b94c10c10fe622429a50b9b4b94ba179746f2acb
matoww <129874812+matoww@users.noreply.github.com>
2024-03-28 08:52:13 +0100

    Apparition d'un tableau Classement

--- Application/Controller/Connexion/AjaxChangementMDP.php
+++ Application/Controller/Connexion/AjaxChangementMDP.php
@@ -2,5 +2,9 @@
 
 include_once "../../Model/Connexion/ConnexionModel.php";
 
-ChangerMDP($_POST["token"],$_POST["newMDP"]);
+try {
+    ChangerMDP($_POST["token"], $_POST["newMDP"]);
+} catch (Exception $e) {
+    echo $e;
+}
 deleteToken($_POST["token"]);
